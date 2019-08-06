f974eb750ccccae37146c276bb2fd2d6bfa54e3e
Blaumeise03 <bluegame61@gmail.com>
2019-08-06 17:23:26 +0200

    Add Copyright
    Signed-off-by: Blaumeise03 <bluegame61@gmail.com>

--- src/main/java/de/blaumeise03/homeWarp/HomeWarp.java
+++ src/main/java/de/blaumeise03/homeWarp/HomeWarp.java
@@ -1,3 +1,21 @@
+
+/*
+ *     Copyright (C) 2019  Blaumeise03 - bluegame61@gmail.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package de.blaumeise03.homeWarp;
 
 import org.bukkit.Bukkit;
@@ -13,11 +31,9 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 public class HomeWarp extends JavaPlugin {
